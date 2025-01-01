f4a23808009a09391a6ea3421138ccec5bd3046f
Mateusz Baginski <cziken58@gmail.com>
2025-01-01 16:31:17 +0100

    Init table layout of timeline

---
+++ src/components/timeline/types.ts
@@ -0,0 +1,24 @@
+export type CompanyColor = {
+  primary: string;
+  background?: string;
+  text?: string;
+};
+
+export interface Company {
+  name: string;
+  logo: string;
+  color: CompanyColor;
+}
+
+export interface ExperienceItem {
+  title: string;
+  company: Company;
+  duration: {
+    begin: Date;
+    end: Date | null;
+  };
+  details: string;
+  items?: string[];
+  projects?: string[];
+  color?: string;
+}
